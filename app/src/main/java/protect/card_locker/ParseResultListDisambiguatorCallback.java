c815e3b93fd0384a908c4fe89ddfce011934c080
Tayphong043 <traingoan043@gmail.com>
2024-12-18 22:36:56 +0700

    Delete app directory

--- app/src/main/java/protect/card_locker/ParseResultListDisambiguatorCallback.java
+++
@@ -1,6 +0,0 @@
-package protect.card_locker;
-
-public interface ParseResultListDisambiguatorCallback {
-    void onUserChoseParseResult(ParseResult parseResult);
-    void onUserDismissedSelector();
-}
--- app/src/main/java/protect/card_locker/ParseResultType.kt
+++
@@ -1,6 +0,0 @@
-package protect.card_locker
-
-enum class ParseResultType {
-    FULL,
-    BARCODE_ONLY
-}
